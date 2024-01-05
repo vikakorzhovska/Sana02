58f9dec963927cc24e57d35bba13b94e3cc0986d
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2024-01-05 12:56:17 +0200

    Completed Task3

---
+++ SwitchValues/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Введіть ціле число у діапазоні [0, 9]");
+int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 0:
+        Console.WriteLine("Нуль");
+        break;
+    case 1:
+        Console.WriteLine("Один");
+        break;
+    case 2:
+        Console.WriteLine("Два");
+        break;
+    case 3:
+        Console.WriteLine("Три");
+        break;
+    case 4:
+        Console.WriteLine("Чотири");
+        break;
+    case 5:
+        Console.WriteLine("П'ять");
+        break;
+    case 6:
+        Console.WriteLine("Шість");
+        break;
+    case 7:
+        Console.WriteLine("Сім");
+        break;
+    case 8:
+        Console.WriteLine("Вісім");
+        break;
+    case 9:
+        Console.WriteLine("Дев'ять");
+        break;
+    default:
+        Console.WriteLine("Число не належить заданому діапазону");
+        break;
+}
